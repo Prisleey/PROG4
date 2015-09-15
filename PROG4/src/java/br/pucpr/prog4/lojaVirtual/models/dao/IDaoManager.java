b069d2841f4296e9b55bfe6499fc4220b441fc9d
Prisleey <prisleey.araujo@hotmail.com>
2015-09-15 14:28:17 -0300

    oi

--- PROG4/src/java/br/pucpr/prog4/lojaVirtual/models/dao/IDaoManager.java
+++ PROG4/src/java/br/pucpr/prog4/lojaVirtual/models/dao/IDaoManager.java
@@ -1,7 +1,6 @@
 package br.pucpr.prog4.lojaVirtual.models.dao;
 
-public interface IDaoManager 
-{
+public interface IDaoManager {
     void iniciar() throws DaoException;
     void encerrar();
     void confirmarTransação();
